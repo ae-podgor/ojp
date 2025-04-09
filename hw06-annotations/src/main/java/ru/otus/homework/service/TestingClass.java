ca9497d8f353717c36fc165a39a4ff6c6b772255
ae.podgor <45737052+ae-podgor@users.noreply.github.com>
2025-04-09 21:41:22 +0300

    hw06-annotations module added (#3)
    * hw06-annotations module added
    
    * hw06-annotations fixes after review
    
    ---------
    
    Co-authored-by: apodgornova <apodgornova@t1.ru>

---
+++ hw06-annotations/src/main/java/ru/otus/homework/service/TestingClass.java
@@ -0,0 +1,37 @@
+package ru.otus.homework.service;
+
+import ru.otus.homework.annotation.After;
+import ru.otus.homework.annotation.Before;
+import ru.otus.homework.annotation.Test;
+
+public class TestingClass {
+
+    @Before
+    public static void initTest() {
+        System.out.println("I am Before test");
+//        throw new RuntimeException();
+    }
+
+    @After
+    public static void finishTest() {
+        System.out.println("I am After test");
+//        throw new RuntimeException();
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("I am Test 1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("I am Test 2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("I am Test 3, throwing an exception");
+        throw new RuntimeException();
+    }
+
+}
