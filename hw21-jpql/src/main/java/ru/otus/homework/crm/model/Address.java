1609a79de9197572a5e1aae21b58f4c9842c7c9b
APodgornova <APodgornova@inno.tech>
2025-06-20 12:58:23 +0300

    hw21-jpql module added

---
+++ hw21-jpql/src/main/java/ru/otus/homework/crm/model/Address.java
@@ -0,0 +1,7 @@
+package ru.otus.homework.crm.model;
+
+public class Address {
+    public Address(Long id, String street) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ hw21-jpql/src/main/java/ru/otus/homework/crm/model/Phone.java
@@ -0,0 +1,7 @@
+package ru.otus.homework.crm.model;
+
+public class Phone {
+    public Phone(Long id, String number) {
+        throw new UnsupportedOperationException();
+    }
+}
