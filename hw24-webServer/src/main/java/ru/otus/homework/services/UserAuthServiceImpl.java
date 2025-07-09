1ada96b1645b91a7a801a6d730245f68c2772c45
APodgornova <APodgornova@inno.tech>
2025-07-09 14:15:48 +0300

    hw24-webServer code review fixes

--- hw24-webServer/src/main/java/ru/otus/homework/services/UserAuthServiceImpl.java
+++ hw24-webServer/src/main/java/ru/otus/homework/services/UserAuthServiceImpl.java
@@ -7,17 +7,16 @@ import java.util.Map;
 
 public class UserAuthServiceImpl implements UserAuthService {
 
-    private final Map<Long, User> users;
+    private final Map<String, User> users;
 
     public UserAuthServiceImpl() {
         users = new HashMap<>();
-        users.put(1L, new User(1L, "Крис Гир", "user1", "password"));
+        users.put("user1", new User(1L, "Крис Гир", "user1", "password"));
     }
 
     @Override
     public boolean authenticate(String login, String password) {
-        return users.values().stream().filter(v -> v.getLogin().equals(login)).findFirst()
-                .map(user -> user.getPassword().equals(password))
-                .orElse(false);
+        User user = users.get(login);
+        return user != null && user.getPassword().equals(password);
     }
 }
