5f1da6b3d12e2e7e00483f5b2032fe785714a892
ae.podgor <45737052+ae-podgor@users.noreply.github.com>
2025-08-04 16:34:33 +0300

    hw32 concurrent collections (#16)
    * hw32-concurrentCollections module added
    
    * hw32-concurrentCollections homework added
    
    * hw32-concurrentCollections changed collection
    
    * hw32-concurrentCollections code review fixes

---
+++ hw32-concurrentCollections/src/main/java/ru/otus/homework/services/processors/SensorDataProcessorBuffered.java
@@ -0,0 +1,58 @@
+package ru.otus.homework.services.processors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.homework.api.SensorDataProcessor;
+import ru.otus.homework.api.model.SensorData;
+import ru.otus.homework.lib.SensorDataBufferedWriter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
+
+// Этот класс нужно реализовать
+@SuppressWarnings({"java:S1068", "java:S125"})
+public class SensorDataProcessorBuffered implements SensorDataProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
+
+    private final int bufferSize;
+    private final SensorDataBufferedWriter writer;
+    private final PriorityBlockingQueue<SensorData> dataBuffer;
+    private final Object lock = new Object();
+
+    public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
+        this.bufferSize = bufferSize;
+        this.writer = writer;
+        this.dataBuffer = new PriorityBlockingQueue<>(bufferSize, Comparator.comparing(SensorData::getMeasurementTime));
+    }
+
+    @Override
+    public void process(SensorData data) {
+        synchronized (lock) {
+            dataBuffer.offer(data);
+            if (dataBuffer.size() >= bufferSize) {
+                flush();
+            }
+        }
+    }
+
+    public void flush() {
+        List<SensorData> bufferedData = new ArrayList<>();
+        dataBuffer.drainTo(bufferedData);
+
+        if (!bufferedData.isEmpty()) {
+            try {
+                writer.writeBufferedData(bufferedData);
+            } catch (Exception e) {
+                log.error("Ошибка в процессе записи буфера", e);
+            }
+        }
+    }
+
+    @Override
+    public void onProcessingEnd() {
+        flush();
+    }
+}
