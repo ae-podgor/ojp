e2fa6a797316ec92ae1bb2d8594b423e88f5cfed
APodgornova <APodgornova@inno.tech>
2025-08-04 18:50:00 +0300

    hw34-multiprocess homework added

---
+++ hw34-multiprocess/src/main/java/ru/otus/homework/NumbersClient.java
@@ -0,0 +1,71 @@
+package ru.otus.homework;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class NumbersClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(NumbersClient.class);
+    private static final int SERVER_PORT = 8190;
+    private static final String SERVER_HOST = "localhost";
+
+    private static int lastServerValue = 0;
+    private static boolean isValueAdded = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(SERVER_HOST, SERVER_PORT)
+                .usePlaintext()
+                .build();
+
+        LOGGER.info("numbers client is starting...");
+        NumberGeneratorGrpc.NumberGeneratorStub stub = NumberGeneratorGrpc.newStub(channel);
+        CountDownLatch latch = new CountDownLatch(1);
+        int currentValue = 0;
+
+        stub.generateNumbers(GenerateRequest.newBuilder().setFirstValue(1).setLastValue(30)
+                        .build(),
+                new StreamObserver<NumberResponse>() {
+                    @Override
+                    public void onNext(NumberResponse response) {
+                        lastServerValue = response.getValue();
+                        isValueAdded = false;
+                        LOGGER.info("new value: '{}'", lastServerValue);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        LOGGER.info("error: ", throwable);
+                        latch.countDown();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        LOGGER.info("request completed");
+                        latch.countDown();
+                    }
+                });
+
+        LOGGER.info("currentValue: '{}'", currentValue);
+        for (int i = 0; i <= 50; i++) {
+            if (!isValueAdded) {
+                currentValue += lastServerValue + 1;
+                isValueAdded = true;
+            } else {
+                currentValue += 1;
+            }
+            LOGGER.info("currentValue: '{}'", currentValue);
+
+            TimeUnit.MILLISECONDS.sleep(1000);
+        }
+
+        latch.await();
+        channel.shutdown();
+    }
+}
+
